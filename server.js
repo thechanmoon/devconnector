0e8cbf7cc4591125aa9a7045f1c0a0a7703f3100
Osx <osx@Osxs-Mac-mini.local>
2022-12-18 19:27:01 -0500

    mongoDB connetion

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+
+// Connect DB
+connectDB();
 
 app.get('/', (req,res) => res.send('API Running'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
